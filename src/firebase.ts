70739185af3b686167db04816504aac6577d9f9e
Moe Frumkin <moegf1@gmail.com>
2022-05-16 22:44:58 -0400

    some fun login boilerplate

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKKmcMKF1UKD3Ib9qHUuAUMlnmxDe5clE",
+    authDomain: "risk-ec844.firebaseapp.com",
+    projectId: "risk-ec844",
+    storageBucket: "risk-ec844.appspot.com",
+    messagingSenderId: "597512603171",
+    appId: "1:597512603171:web:bb84a92c5afc2baf2f1396",
+    measurementId: "G-ZQJYT8M3N2"
+}
+
+export const initFirebase = () => {
+    const app = firebase.initializeApp(firebaseConfig)
+    const auth = getAuth(app)
+
+    return { auth }
+}
