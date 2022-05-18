f672133ab84fb2c1a2d1d6827bdc8bcd188faa03
Moe Frumkin <moegf1@gmail.com>
2022-05-18 14:45:28 -0400

    add login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { User } from '../providers/user';
 
 export const Header = () => {
-    const {user} = User.useContainer()
+    const {user, auth} = User.useContainer()
 
     return (
         <header>
             <div>
                 <Link to="/">Risk</Link>
-                {user ? 
-                    <Link to="/login">Login</Link> <Link to="signup">Sign Up</Link>}
+                {user ?
+                    <Link to="/" onClick = {() => {auth.signOut()}}>Sign Out</Link>:
+                    <Fragment>
+                        <Link to="/login">Login</Link> <Link to="signup">Sign Up</Link>
+                    </Fragment>
+                }
                 <Link to="/rules">Rules </Link>
                 <Link to="/play">Play</Link>
             </div>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'firebase/firestore'
 import 'firebase/auth'
+import { User } from '../providers/user';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Header } from './Header';
 
 export const Login = () => {
+    const {auth} = User.useContainer();
 
+    let [error, setError] = useState<Error | null>()
+    let [email, setEmail] = useState("")
+    let [password, setPassword] = useState("")
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        signInWithEmailAndPassword(auth,email, password)
+            .catch(error => {
+                setError(error)
+            })
+    }
 
     return (
         <div>
-            hi
+            <Header/>
+            <p>Login</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email:
+                    <input type={"text"} onChange={event => setEmail(event.target.value)}/>
+                </label>
+                <label>
+                    Password:
+                    <input type={"password"} onChange={event => setPassword(event.target.value)}/>
+                </label>
+                <input type={"submit"}/>
+            </form>
+            {error ?
+                <p>An error occurred: {error.message}</p> :
+                <p>All is good</p>
+            }
         </div>
     )
 }
