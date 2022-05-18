7e80d5c38714d7b073a697574c7232a393660d39
Moe Frumkin <moegf1@gmail.com>
2022-05-18 14:30:56 -0400

    example function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,17 @@
+import { firestore } from "firebase-admin";
 import * as functions from "firebase-functions";
+const admin =  require("firebase-admin")
+
+admin.initializeApp()
+
+export const createGame = functions.https.onCall((data, context) => {
+    firestore().collection("games/").doc(data.name).set({
+        name: data.name,
+        host: context.auth?.uid 
+    })
+
+    return {}
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
